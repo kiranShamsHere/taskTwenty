63c95a16a0d713483d7ff75d95f3de76cb273f6f
Kiran Shams <160856436+kiranShamsHere@users.noreply.github.com>
2024-04-03 22:59:53 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+function day() {
+  const currentDay = new Date().getDay();
+  let dayName;
+
+  switch (currentDay) {
+    case 0:
+      dayName = "Sunday";
+      break;
+    case 1:
+      dayName = "Monday";
+      break;
+    case 2:
+      dayName = "Tuesday";
+      break;
+    case 3:
+      dayName = "Wednesday";
+      break;
+    case 4:
+      dayName = "Thursday";
+      break;
+    case 5:
+      dayName = "Friday";
+      break;
+    case 6:
+      dayName = "Saturday";
+      break;
+    default:
+      dayName = "Invalid day";
+  }
+  document.getElementById("day").innerText = `Today is ${dayName}`;
+}
